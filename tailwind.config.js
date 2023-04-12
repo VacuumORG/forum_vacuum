7c2ea3acff35de0c1d0242e8cdea5ad8bbd6d6e6
José Oliveira <jose.oliveira@integer.pt>
2023-04-12 13:59:26 -0300

    :sparkles: build: add tailwind css and base files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+    './src/pages/**/*.{js,jsx,ts,tsx}',
+    './src/styles/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
