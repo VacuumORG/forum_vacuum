1dd1f3836fcbe16da8023fae77dc600754f7c5d7
Henrique Chaves <henriquechaves.jobs@gmail.com>
2023-04-18 19:25:31 -0300

    feat: create user avatar component

---
+++ src/components/UserAvatar/index.tsx
@@ -0,0 +1,22 @@
+import Image, { StaticImageData } from 'next/image'
+import { FunctionComponent } from 'react'
+
+interface UserAvatarProps {
+  img: string | StaticImageData
+  alf: string
+  width?: number
+  height?: number
+}
+
+const UserAvatar: FunctionComponent<UserAvatarProps> = ({
+  img,
+  alf,
+  width = 36,
+  height = 36
+}: UserAvatarProps) => {
+  return (
+    <Image className="rounded-[50%]" width={width} height={height} src={img} alt={alf} />
+  )
+}
+
+export default UserAvatar
