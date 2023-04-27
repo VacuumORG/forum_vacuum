f2958520a17962569b89fa8bc1a5e541e9a30152
Henrique Chaves <henriquechaves.jobs@gmail.com>
2023-04-27 17:10:04 -0300

    feat: add border when component is on focus event

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,15 +1,13 @@
-import Image from 'next/image'
-import { FunctionComponent, HTMLInputTypeAttribute } from 'react'
+import { FunctionComponent, HTMLInputTypeAttribute, useState } from 'react'
 
 interface TextFieldProps {
   id?: string
   label?: string
   placeholder?: string
-  icon: string
+  icon?: JSX.Element
   children?: JSX.Element
   type?: HTMLInputTypeAttribute
   required?: boolean
-  hiddenIcon?: boolean
 }
 
 const TextField: FunctionComponent<TextFieldProps> = ({
@@ -20,22 +18,20 @@ const TextField: FunctionComponent<TextFieldProps> = ({
   children,
   type,
   required,
-  hiddenIcon = false,
 }: TextFieldProps) => {
+  const [border, setBorder] = useState('1px solid transparent')
   return (
     <div className="flex flex-col">
       <label htmlFor={id} className="font-bold text-xs theme-text mb-1">
         {label}
       </label>
-      <div className="flex p-1 theme-textfield-icon-box visited:bg-slate-950 rounded">
-        <Image
-          hidden={hiddenIcon}
-          className="mr-1"
-          src={icon}
-          alt="icon"
-          width={24}
-          height={24}
-        />
+      <div
+        className="flex p-1 theme-textfield-icon-box visited:bg-slate-950 rounded"
+        style={{
+          border: border,
+        }}
+      >
+        {icon}
         <input
           type={type}
           name="input"
@@ -43,6 +39,12 @@ const TextField: FunctionComponent<TextFieldProps> = ({
           className="focus:outline-none items-stretch w-full px-2 text-xs"
           placeholder={placeholder}
           required={required}
+          onFocus={() => {
+            setBorder('1px solid var(--default)')
+          }}
+          onBlur={() => {
+            setBorder('1px solid transparent')
+          }}
         />
         {children}
       </div>
