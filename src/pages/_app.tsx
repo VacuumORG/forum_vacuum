09f8fdaed8108f84320b1c90c733b8fd7c29e22b
Henrique Chaves <henriquechaves.jobs@gmail.com>
2023-04-27 02:03:30 -0300

    feat: add default fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
 import type { AppProps } from 'next/app'
+import { Montserrat } from 'next/font/google'
 import '@/styles/global.css'
 import '@/styles/theme.css'
 import '@/styles/dark.css'
-
+const montserrat = Montserrat({
+  weight: ['400', '500', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+})
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${montserrat.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
