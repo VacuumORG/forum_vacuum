1f97d04a1a1e96d85db640186a579bd2a4131acd
Diegiwg <diegiwg@gmail.com>
2023-05-04 00:59:24 -0300

    Feat: [new] User authentication route
    - Criada a rota de autenticação do usuário.
      - Foi utilizado o module ["Authentication"](https://supabase.com/docs/guides/auth#authentication) do Supabase.
      - Foi criado um endpoint para autenticar o usuário.
        - /api/auth -> é utilizando o Email e Senha.

---
+++ src/pages/api/auth/index.ts
@@ -0,0 +1,37 @@
+import { NextApiResponse, NextApiRequest } from 'next'
+import { supabase } from '../db'
+
+type LoginBody = {
+  email: string
+  password: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' })
+  }
+
+  const { email, password }: LoginBody = req.body
+  if (!email || !password) {
+    return res.status(400).json({
+      message: 'Missing email or password',
+    })
+  }
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  })
+
+  if (!error) {
+    return res.status(200).json({ data })
+  }
+
+  return res.status(error.status!).json({
+    type: error.name,
+    message: error.message,
+  })
+}
