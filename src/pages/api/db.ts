af6a0b07b083e3944614302fa2b133b4ac2c2252
Diegiwg <diegiwg@gmail.com>
2023-05-04 02:25:01 -0300

    fix/padronizando o acesso ao supabase
    Conforme foi solicitado em:
    https://github.com/VacuumORG/forum_vacuum_front/pull/30
    https://github.com/VacuumORG/forum_vacuum_front/pull/28
    
    O arquivo db.ts foi movido para /src/lib/ e renomeado para connection.ts

--- src/pages/api/db.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseSecretKey = process.env.SUPABASE_KEY
-
-export const supabase = createClient(supabaseUrl!, supabaseSecretKey!)
