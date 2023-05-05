0404b53d65de56eb453fcc00740546c057bc1dd4
lfs <luciano2023silva@gmail.com>
2023-05-05 00:53:38 -0300

    lint: run prettier fixer

--- src/pages/api/v1/tags/index.ts
+++ src/pages/api/v1/tags/index.ts
@@ -1,9 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { supabase } from '~/lib/connection'
 
-
-
-export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   let { data: tags, error } = await supabase.from('tags').select('id, name')
 
   if (error) {
