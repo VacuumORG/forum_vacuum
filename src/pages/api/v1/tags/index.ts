b4c1b05b7bb01878f3854bb921e0cb915a2e68b9
Diegiwg <diegiwg@gmail.com>
2023-05-05 18:28:13 -0300

    Improving the Algorithm and Implementing the StatusCode Usage

--- src/pages/api/v1/tags/index.ts
+++ src/pages/api/v1/tags/index.ts
@@ -1,17 +1,21 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { supabase } from '~/lib/connection'
+import { CodeClientError, CodeServerError, CodeSuccess } from '~/lib/statusCode'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  let { data: tags, error } = await supabase.from('tags').select('id, name')
-
   if (req.method !== 'GET') {
-    return res.status(405).json({ message: 'Method not allowed' })
+    return res
+      .status(CodeClientError.MethodNotAllowed)
+      .json({ message: 'Method not allowed' })
   }
+
+  const { data: tags, error } = await supabase.from('tags').select('id, name')
+
   if (error) {
-    return res.status(404).json(error)
+    return res.status(CodeServerError.InternalServerError).json(error)
   }
-  return res.status(200).json(tags)
+  return res.status(CodeSuccess.OK).json(tags)
 }
