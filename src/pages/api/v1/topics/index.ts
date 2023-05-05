9becb2d233217d03d5d8fed96773b0f0a1e99bf0
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-05 00:49:17 -0300

    feat: add params in route topics

--- src/pages/api/v1/topics/index.ts
+++ src/pages/api/v1/topics/index.ts
@@ -1,11 +1,25 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { supabase } from '~/lib/connection'
+import { TopicModel } from '~/models/topics/TopicModel'
 
 export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { data: topics, error } = await supabase.from('topics').select('*')
+  const {
+    qtdTopics = 20,
+    orderBy = 'created_at',
+    startTopic = 0,
+  } = _req.query as TopicModel
+
+  if (_req.method != 'GET') {
+    return res.status(405).json({ message: 'Method is not allowed' })
+  }
+  const { data: topics, error } = await supabase
+    .from('topics')
+    .select('*')
+    .order(`${orderBy}`, { ascending: true })
+    .range(startTopic, startTopic + qtdTopics - 1)
 
   if (error) {
     return res.json(error)
