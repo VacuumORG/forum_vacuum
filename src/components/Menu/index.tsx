d87884020af1d0a756a99ecaf2e9983331a01daf
Henrique Chaves <henriquechaves.jobs@gmail.com>
2023-05-06 06:28:43 -0300

    refactor: add children to receive user area component and add className props

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,13 +2,15 @@ import { FunctionComponent, useState } from 'react'
 import TextField from '../TextField'
 import Image from 'next/image'
 import IconButton from '../IconButton'
-import UserAvatar from '../UserAvatar'
 
-interface MenuProps {}
+interface MenuProps {
+  children?: JSX.Element | JSX.Element[]
+  className?: string
+}
 
-const Menu: FunctionComponent<MenuProps> = () => {
+const Menu: FunctionComponent<MenuProps> = ({ className, children }) => {
   return (
-    <header className="flex items-center justify-between flex-wrap px-20 theme-menu">
+    <header className={className}>
       <div className="flex items-center flex-shrink-0 mr-6">
         <Image width={90} height={90} src="/vacuum_logo.png" alt="" />
         <span className="font-montserrat font-bold text-2xl leading-10">
@@ -16,21 +18,13 @@ const Menu: FunctionComponent<MenuProps> = () => {
         </span>
       </div>
 
-      <form action="#" className="w-1/2 flex flex-col gap-4">
+      <form className="w-1/2 flex flex-col gap-4">
         <TextField id="search" type={'text'} placeholder="Search" required>
           <IconButton icon={'/icon_search.svg'} w={24} h={24} />
         </TextField>
       </form>
 
-      <div className="flex items-center">
-        <div className="flex-shrink-0">
-          <UserAvatar alf="User avatar" img={'/user_avatar.png'} />
-        </div>
-        <div className="ml-3 font-bold flex flex-col">
-          Bem vindo,
-          <span className="text-sm theme-text-purple">offKevyn</span>
-        </div>
-      </div>
+      {children}
     </header>
   )
 }
