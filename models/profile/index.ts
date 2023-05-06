537a1e7db868b0ef531f81e93d512403544a885f
Diegiwg <diegiwg@gmail.com>
2023-05-06 15:21:57 -0300

    feat/list-all-topics-for-a-user
    TODO: implementar proteção com autenticação
    
    |PATH|HTTP VERB|REQUEST|RESPONSE|
    |---|---|---|---|
    |/api/v1/profile/topics|GET|JSON {user_id:uuid}|JSON{topics:[]}|

--- models/profile/index.ts
+++ models/profile/index.ts
@@ -1,6 +1,12 @@
+import { UUID } from 'crypto'
+
 export interface UserProfileModel {
   name: string
   email: string
   password: string
   avatar_url: string
+}
+
+export interface UserIdModel {
+  user_id: UUID
 }
