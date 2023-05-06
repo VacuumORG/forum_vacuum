de2cfd845ce7d1425f443c990561150ba6d64209
Henrique Chaves <henriquechaves.jobs@gmail.com>
2023-05-06 07:01:43 -0300

    feat: add props function to handler any event when user click

--- src/components/UserArea/index.tsx
+++ src/components/UserArea/index.tsx
@@ -1,23 +1,31 @@
 import { UserCircle } from '@phosphor-icons/react'
-import { FunctionComponent } from 'react'
+import { FunctionComponent, MouseEventHandler } from 'react'
 
 interface UserAreaProps {
   message?: string
   userName?: string
   icon?: JSX.Element
+  onClickEventOne?: MouseEventHandler<HTMLButtonElement>
+  onClickEventTwo?: MouseEventHandler<HTMLButtonElement>
 }
 
 const UserArea: FunctionComponent<UserAreaProps> = ({
   message = 'entre',
   userName = 'cadastra-se',
   icon = <UserCircle width={45} height={45} />,
+  onClickEventOne,
+  onClickEventTwo,
 }) => {
   return (
     <div className="flex items-center">
       <div className="flex-shrink-0">{icon}</div>
       <div className="ml-3 font-bold flex flex-col items-start">
-        <button className="text-sm font-bold">{message}</button>
-        <button className="text-sm theme-text-purple">{userName}</button>
+        <button onClick={onClickEventOne} className="text-sm font-bold">
+          {message}
+        </button>
+        <button onClick={onClickEventTwo} className="text-sm theme-text-purple">
+          {userName}
+        </button>
       </div>
     </div>
   )
