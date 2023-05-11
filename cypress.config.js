29859b28a983c5d93b82a24c982fe18c66e6e00b
Henrique Chaves <henriquechaves.jobs@gmail.com>
2023-05-11 02:51:37 -0300

    build: add new configuration on cypress to work with typescript

--- cypress.config.js
+++ cypress.config.js
@@ -3,17 +3,19 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   projectId: 'i7b9k1',
   watchForFileChanges: true,
-
   component: {
     devServer: {
       framework: 'next',
       bundler: 'webpack',
     },
-    supportFile: false,
-    specPattern: 'cypress/components/*.spec.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/component.ts',
+    specPattern: ['cypress/**/*.cy.{js,jsx,ts,tsx}'],
   },
 
   e2e: {
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/tests/**/*.spec.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.ts',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
