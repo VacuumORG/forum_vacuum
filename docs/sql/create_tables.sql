1c4668d16d5983a1a9e91ac6808d82dba8f971f6
Diegiwg <diegiwg@gmail.com>
2023-05-12 23:05:11 -0300

    feat/add-sql-to-create-tables

---
+++ docs/sql/create_tables.sql
@@ -0,0 +1,73 @@
+create table profiles (
+  id uuid references auth.users on delete cascade not null primary key,
+  created_at timestamp with time zone default timezone('utc'::text, now()) not null,
+  updated_at timestamp with time zone default timezone('utc'::text, now()) not null,
+  name text unique,
+  avatar_url text
+);
+
+create table topics (
+  id uuid  default uuid_generate_v4() primary key,
+  user_id uuid references public.profiles on delete cascade not null,
+  created_at timestamp with time zone default timezone('utc'::text, now()) not null,
+  updated_at timestamp with time zone default timezone('utc'::text, now()) not null,
+  title text not null,
+  content text not null,
+  views int4 not null default 0,
+  likes int4 not null default 0,
+  tags jsonb,
+  images jsonb,
+  docs jsonb
+);
+
+create table comments (
+  id uuid  default uuid_generate_v4() primary key,
+  user_id uuid references public.profiles on delete cascade not null,
+  topic_id uuid references public.topics on delete cascade not null,
+  created_at timestamp with time zone default timezone('utc'::text, now()) not null,
+  updated_at timestamp with time zone default timezone('utc'::text, now()) not null,
+  content text not null,
+  likes int4 not null default 0
+);
+
+-- Extra configs for Auth Module
+
+-- Set up Row Level Security (RLS)
+-- See https://supabase.com/docs/guides/auth/row-level-security for more details.
+alter table profiles
+  enable row level security;
+
+create policy "Public profiles are viewable by everyone." on profiles
+  for select using (true);
+  
+create policy "Users can insert their own profile." on profiles
+  for insert with check (auth.uid() = id);
+
+create policy "Users can update own profile." on profiles
+  for update using (auth.uid() = id);
+
+-- This trigger automatically creates a profile entry when a new user signs up via Supabase Auth.
+-- See https://supabase.com/docs/guides/auth/managing-user-data#using-triggers for more details.
+create function public.handle_new_user()
+returns trigger as $$
+begin
+  insert into public.profiles (id, name, avatar_url)
+  values (new.id, new.raw_user_meta_data->>'name', new.raw_user_meta_data->>'avatar_url');
+  return new;
+end;
+$$ language plpgsql security definer;
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
+
+-- Set up Storage!
+insert into storage.buckets (id, name)
+  values ('avatars', 'avatars');
+
+-- Set up access controls for storage.
+-- See https://supabase.com/docs/guides/storage#policy-examples for more details.
+create policy "Avatar images are publicly accessible." on storage.objects
+  for select using (bucket_id = 'avatars');
+
+create policy "Anyone can upload an avatar." on storage.objects
+  for insert with check (bucket_id = 'avatars');
