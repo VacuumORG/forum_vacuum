6d2f38b1edb51055a5f5ffb7e372df6904cf823c
lfs <luciano2023silva@gmail.com>
2023-05-14 02:18:29 -0300

    feat: Rota que retorna uma tag especifica passando o id e Rota que retorna uma lista de tags

--- src/pages/api/v1/tags/[id]/index.ts
+++ src/pages/api/v1/tags/[id]/index.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { supabase } from '~/lib/connection'
 import { CodeClientError, CodeServerError, CodeSuccess } from '~/lib/statusCode'
+import { GetATagModel } from '~/models/tag'
 
 export default async function handler(
   _req: NextApiRequest,
@@ -12,10 +13,12 @@ export default async function handler(
       .json({ message: 'Method not allowed' })
   }
 
-  const { data: tags, error } = await supabase.from('tags').select('id, name')
+  const { id: tagId } = _req.query as unknown as GetATagModel
+
+  const { data: tags, error } = await supabase.from('tags').select('id, name').eq('id', tagId!)
 
   if (error) {
     return res.status(CodeServerError.InternalServerError).json(error)
   }
-  return res.status(CodeSuccess.OK).json(tags)
+  return res.status(CodeSuccess.OK).json(tags[0])
 }
---
+++ src/pages/api/v1/tags/spec.md
@@ -0,0 +1,6 @@
+# Perfil - Spec
+
+| Verbos | Protegida | Rota                                  | Descrição                                                                             |
+| ------ | --------- | ------------------------------------- | ------------------------------------------------------------------------------------- |
+| GET    | Não       | /api/v1/tags                      | Esta rota retorna uma lista de tags.                                                |
+| GET   | Não       | /api/v1/profiles/[id]                      | Esta rota retorna uma tag com o id especifíco                                |
