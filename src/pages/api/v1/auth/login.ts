377c31e1ae2d5bdbe3007ca55f525ef248bbc404
Diegiwg <diegiwg@gmail.com>
2023-05-15 22:24:51 -0300

    fix/broken-routes

--- src/pages/api/v1/auth/login.ts
+++ src/pages/api/v1/auth/login.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { supabase } from '~/lib/connection'
 import { CodeClientError, CodeServerError, CodeSuccess } from '~/lib/statusCode'
-import { LoginModel } from '~/models/auth'
+import { LoginModel, TokenResponseModel } from '~/models/auth'
 
 export default async function handler(
   _req: NextApiRequest,
@@ -30,5 +30,12 @@ export default async function handler(
     return res.status(CodeServerError.InternalServerError).json(error)
   }
 
-  return res.status(CodeSuccess.OK).json(data)
+  const token = {
+    user_id: data.user!.id,
+    refresh_token: data.session!.refresh_token,
+    access_token: data.session!.access_token,
+    token_type: data.session!.token_type,
+  } as unknown as TokenResponseModel
+
+  return res.status(CodeSuccess.OK).json(token)
 }
--- src/pages/api/v1/profiles/create.ts
+++ src/pages/api/v1/profiles/create.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { supabase } from '~/lib/connection'
-import { guard } from '~/lib/httpVerbGuard'
 import { CodeClientError, CodeServerError, CodeSuccess } from '~/lib/statusCode'
 import { TokenResponseModel } from '~/models/auth'
 import { UserCreateModel } from '~/models/profile'
@@ -9,9 +8,6 @@ export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const resolved = guard(_req, res, 'POST')
-  if (!resolved) return
-
   const { email, password, name } = _req.body as unknown as UserCreateModel
   if (!email || !password || !name) {
     return res.status(CodeClientError.BadRequest).json({
