6aea5d19c1817382bda8fc7cf492913df8ac2cda
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-23 07:37:09 -0300

    fix: fixed route for many types

--- src/pages/api/v1/topics/index.ts
+++ src/pages/api/v1/topics/index.ts
@@ -4,29 +4,47 @@ import { CodeClientError, CodeServerError, CodeSuccess } from '~/lib/statusCode'
 import { ListAllTopicsModel } from '~/models/topic'
 
 export default async function handler(
-  _req: NextApiRequest,
+  req: NextApiRequest,
   res: NextApiResponse
 ) {
+  let response
+
+  switch (req.method) {
+    case 'GET':
+      response = await getTopics(req)
+      break
+
+    case 'POST':
+      response = await createTopic(req)
+      break
+
+    default:
+      return res
+        .status(CodeClientError.MethodNotAllowed)
+        .json({ message: 'Method not allowed' })
+  }
+
+  if (response.error) {
+    return res.status(CodeServerError.InternalServerError).json(response.error)
+  }
+
+  return res.status(CodeSuccess.OK).json(response.data)
+}
+
+async function getTopics(req: NextApiRequest){
   const {
     qtdTopics = 20,
     orderBy = 'created_at',
     startTopic = 0,
-  } = _req.query as ListAllTopicsModel
+  } = req.query as ListAllTopicsModel
 
-  if (_req.method != 'GET') {
-    return res
-      .status(CodeClientError.MethodNotAllowed)
-      .json({ message: 'Method is not allowed' })
-  }
-  const { data: topics, error } = await supabase
+  const { data, error } = await supabase
     .from('topics')
     .select('*')
     .order(`${orderBy}`, { ascending: true })
     .range(startTopic, startTopic + qtdTopics - 1)
 
-  if (error) {
-    return res.status(CodeServerError.InternalServerError).json(error)
-  }
-
-  return res.status(CodeSuccess.OK).json(topics)
+  return {data, error}
+}
+async function createTopic(req: NextApiRequest){
 }
