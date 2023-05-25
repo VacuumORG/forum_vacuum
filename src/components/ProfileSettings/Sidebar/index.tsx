b36cf0d09188368620b735f553b498ecf47f30a1
lfs <luciano2023silva@gmail.com>
2023-05-25 19:21:59 -0300

    fix: body elements and body and style UploadAvatar

--- src/components/ProfileSettings/Sidebar/index.tsx
+++ src/components/ProfileSettings/Sidebar/index.tsx
@@ -1,40 +1,20 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import UploadAvatar from './UploadAvatar'
+import EditUserContainer from '../EditUserContainer'
 
 export default function Sidebar() {
   return (
-    <section className="max-w-[250px] h-[18.350rem] -mt-9">
-      <div className="flex items-center gap-6">
-        <Image
-          src="/unsplashAvatar.jpg"
-          alt="avatar"
-          width={115}
-          height={115}
-          className="object-cover rounded-full relative"
-        />
-
-        <h2 className="font-bold text-2xl whitespace-nowrap">
-          John Silvio Santos Costa Doe
-        </h2>
+    <section className="h-[18.350rem] flex justify-center gap-6">
+      <div className="">
+        <h2 className="font-bold text-xl">Em Geral</h2>
+        <div className="flex flex-col pt-[2.8125rem]">
+          <Link href="" className="font-bold text-[0.8125rem] text-default">
+            Editar informações
+          </Link>
+        </div>
       </div>
-      <h2 className="font-bold text-xl mt-[2.6875rem]">Em Geral</h2>
-      <div className="flex flex-col pt-[2.8125rem]">
-        <Link href="" className="font-bold text-[0.8125rem] text-default">
-          Editar informações
-        </Link>
-        {/**
-         * <Link
-          href=""
-        >
-          Atividade
-        </Link>
-        <Link
-          href=""
-        >
-          Comentários
-        </Link>
-         */}
-      </div>
+      <EditUserContainer />
     </section>
   )
 }
