a9b2a90cd14c5abf7537d51bbf5acaa7c8673b3f
lfs <luciano2023silva@gmail.com>
2023-05-25 19:21:59 -0300

    fix: body elements and body and style UploadAvatar

--- src/components/ProfileSettings/index.tsx
+++ src/components/ProfileSettings/index.tsx
@@ -1,11 +1,18 @@
 import Sidebar from './Sidebar'
 import EditUserContainer from './EditUserContainer'
+import UploadAvatar from './Sidebar/UploadAvatar'
 
 export default function ProfileSettings() {
   return (
-    <div className="flex">
+    <div className="flex flex-col w-full gap-8">
+      <div className="flex items-center justify-center gap-6 w-[75%]">
+        <UploadAvatar />
+
+        <h2 className="font-bold text-2xl whitespace-nowrap">
+          John Silvio Santos Costa Doe
+        </h2>
+      </div>
       <Sidebar />
-      <EditUserContainer />
     </div>
   )
 }
