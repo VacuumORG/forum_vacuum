9c5cf8ef6d6b993163ac936269a947b942847da4
lfs <luciano2023silva@gmail.com>
2023-05-26 00:34:08 -0300

    lint: fix lint

--- src/services/axiosClient.ts
+++ src/services/axiosClient.ts
@@ -1,5 +1,5 @@
-import axios from 'axios';
+import axios from 'axios'
 
 export const axiosClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_BASE_URL
+  baseURL: process.env.NEXT_PUBLIC_SERVER_BASE_URL,
 })
