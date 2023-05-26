a1988b8013ddb9ca82b5bc986bf7db6e5e05e4f6
Dayane <dayanecarolina.oliveira@gmail.com>
2023-05-26 14:32:38 -0300

    fix: remove consoles

--- src/api/services/auth.ts
+++ src/api/services/auth.ts
@@ -6,10 +6,8 @@ export const login = async (loginModel: LoginModel): Promise<string> => {
   try {
     const response = await api.post('/auth/login/', loginModel)
     const userData = response.data
-    console.log('userData', userData.token)
     const token = userData.token
     setCookie('token', token)
-    console.log(token)
     return token
   } catch (error) {
     throw new Error('Usuário ou senha inválidos')
